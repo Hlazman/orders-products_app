0993b1a861314246531b3784ddd00f6bea2c09dc
Hlazman <glazman.b@gmail.com>
2023-05-02 12:15:44 +0300

    changed component NavigationMenu

--- client/src/components/NavigationMenu.jsx
+++ client/src/components/NavigationMenu.jsx
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavigationMenu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light min-vh-100 align-items-baseline">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light align-items-baseline" style={{ height: showMenu ? 'auto' : '100vh' }}>
+
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        onClick={toggleMenu}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse m-3" id="navbarNav">
+      <div className={`collapse navbar-collapse m-3 ${showMenu ? 'show' : ''}`} id="navbarNav">
         <ul className="navbar-nav flex-column">
           <li className="nav-item">
-            <NavLink to="/orders" className="nav-link">
+            <NavLink to="/orders" className="nav-link" onClick={toggleMenu}>
               Orders
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/products" className="nav-link">
+            <NavLink to="/products" className="nav-link" onClick={toggleMenu}>
               Products
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/create" className="nav-link">
+            <NavLink to="/create" className="nav-link" onClick={toggleMenu}>
               Create order
             </NavLink>
           </li>
@@ -39,3 +42,7 @@ const NavigationMenu = () => {
 };
 
 export default NavigationMenu;
+
+
+
+
